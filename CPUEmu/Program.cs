bce5cf6730758c50a67b021f04918a7d6d2ff08f
Akhil Rangaraj <akhilrangaraj@users.noreply.github.com>
2018-08-11 12:54:37 -0700

    Add some scaffolding stuff, target .net core 2.1 because its the new hotness

---
+++ CPUEmu/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using x8086;
+
+namespace CPUEmu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            core cpu = new core();
+        }
+    }
+}
--- x8086/core.cs
+++ x8086/core.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace x8086
 {
-    class core
+    public class core
     {
     }
 }
